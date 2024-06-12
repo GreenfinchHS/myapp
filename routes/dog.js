515baa598530ecd950003933a5cb6760d6c1adb2
Katagiri <soybeans058@gmail.com>
2024-06-13 06:27:33 +0900

    Add dog route and view

--- routes/dog.js
+++ routes/dog.js
@@ -1,14 +1,18 @@
+// dog.js
+
 var express = require('express');
 var router = express.Router();
 const request = require('request');
-router.get('/', async (req, res) => {
-request('https://dog.ceo/api/breeds/image/random', function (error, response,
-body) {
-if (!error && response.statusCode == 200) {
-const data = JSON.parse(body);
-res.json(data);
-}
+
+router.get('/', function(req, res, next) {
+    request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+            res.render('dog', { imageURL: data.message }); // dog.jade をレンダリングし、画像URLを渡す
+        } else {
+            next(error); // エラーがあれば次のミドルウェアに処理を渡す
+        }
+    });
 });
-})
 
 module.exports = router;
